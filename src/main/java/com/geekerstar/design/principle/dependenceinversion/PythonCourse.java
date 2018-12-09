64a4bfc78a457c776431a53bc8fd9471d9622ef7
geekerstar <247507792@qq.com>
2018-12-09 15:36:14 +0800

    依赖倒置原则

---
+++ src/main/java/com/geekerstar/design/principle/dependenceinversion/PythonCourse.java
@@ -0,0 +1,13 @@
+package com.geekerstar.design.principle.dependenceinversion;
+
+/**
+ * @author geekerstar
+ * @date 2018/12/9
+ * description
+ */
+public class PythonCourse implements ICourse {
+    public void studyCourse() {
+        System.out.println("geeker在学Python");
+
+    }
+}
