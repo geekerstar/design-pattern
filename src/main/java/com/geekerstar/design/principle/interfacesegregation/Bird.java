364400b5b7685964c4edf5735d03bb89220c0258
geekerstar <247507792@qq.com>
2019-01-06 15:44:00 +0800

    接口隔离原则

---
+++ src/main/java/com/geekerstar/design/principle/interfacesegregation/Bird.java
@@ -0,0 +1,20 @@
+package com.geekerstar.design.principle.interfacesegregation;
+
+/**
+ * @author geekerstar
+ * date: 2019/1/6 15:39
+ * description:
+ */
+public class Bird implements IAnimalAction {
+    public void eat() {
+
+    }
+
+    public void fly() {
+
+    }
+
+    public void swim() {
+
+    }
+}
---
+++ src/main/java/com/geekerstar/design/principle/interfacesegregation/Dog.java
@@ -0,0 +1,27 @@
+package com.geekerstar.design.principle.interfacesegregation;
+
+/**
+ * @author geekerstar
+ * date: 2019/1/6 15:38
+ * description:
+ */
+public class Dog implements ISwimAnimalAction,IEatAnimalAction {
+    public void eat() {
+
+    }
+
+    public void swim() {
+
+    }
+//    public void eat() {
+//
+//    }
+//
+//    public void fly() {
+//
+//    }
+//
+//    public void swim() {
+//
+//    }
+}
