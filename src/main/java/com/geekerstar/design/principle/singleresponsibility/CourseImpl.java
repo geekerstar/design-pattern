56ca68094d5ddb355713ab1800733bdff97bfa75
geekerstar <247507792@qq.com>
2019-01-06 15:29:54 +0800

    单一职责原则

---
+++ src/main/java/com/geekerstar/design/principle/singleresponsibility/CourseImpl.java
@@ -0,0 +1,24 @@
+package com.geekerstar.design.principle.singleresponsibility;
+
+/**
+ * @author geekerstar
+ * date: 2019/1/6 15:22
+ * description:
+ */
+public class CourseImpl implements ICourseManager,ICourseContent {
+    public void studyCourse() {
+
+    }
+
+    public void refundCourse() {
+
+    }
+
+    public String getCourseName() {
+        return null;
+    }
+
+    public byte[] getCourseVideo() {
+        return new byte[0];
+    }
+}
