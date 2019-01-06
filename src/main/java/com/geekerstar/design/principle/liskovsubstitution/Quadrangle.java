317d19e5f3a3065d1e82fec76f0fccd2df2ed354
geekerstar <247507792@qq.com>
2019-01-06 16:52:27 +0800

    里氏替换原则

---
+++ src/main/java/com/geekerstar/design/principle/liskovsubstitution/Quadrangle.java
@@ -0,0 +1,11 @@
+package com.geekerstar.design.principle.liskovsubstitution;
+
+/**
+ * @author geekerstar
+ * date: 2019/1/6 16:34
+ * description:
+ */
+public interface Quadrangle {
+    long getWidth();
+    long getLength();
+}
---
+++ src/main/java/com/geekerstar/design/principle/liskovsubstitution/Square.java
@@ -0,0 +1,55 @@
+package com.geekerstar.design.principle.liskovsubstitution;
+
+/**
+ * @author geekerstar
+ * date: 2019/1/6 16:24
+ * description:
+ */
+public class Square implements Quadrangle {
+    private long sideLength;
+
+//    public long getSideLength() {
+//        return sideLength;
+//    }
+//
+//    public void setSideLength(long sideLength) {
+//        this.sideLength = sideLength;
+//    }
+//
+//    @Override
+//    public long getLength() {
+//        return getSideLength();
+//    }
+//
+//    @Override
+//    public void setLength(long length) {
+//        setSideLength(length);
+//    }
+//
+//    @Override
+//    public long getWidth() {
+//        return getSideLength();
+//    }
+//
+//    @Override
+//    public void setWidth(long width) {
+//        setSideLength(width);
+//    }
+
+
+    public long getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(long sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public long getWidth() {
+        return sideLength;
+    }
+
+    public long getLength() {
+        return sideLength;
+    }
+}
